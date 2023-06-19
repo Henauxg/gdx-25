d02e4eda512445f5f868b48d9781a5c420a678a7
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-06-19 12:05:15 +0200

    Add libGDX generated project files

---
+++ desktop/src/fr/baldurcrew/gdx25/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package fr.baldurcrew.gdx25;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import fr.baldurcrew.gdx25.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("gdx-jam-25");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
