df2b7f7658a3315223aaad3c67d572b3b334b8bf
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-06-21 19:02:29 +0200

    Increase viewport size a bit

--- core/src/fr/baldurcrew/gdx25/Constants.java
+++ core/src/fr/baldurcrew/gdx25/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
     public static final int VELOCITY_ITERATIONS = 6;
         public static final int POSITION_ITERATIONS = 2;
     public static final int GRAVITY_VALUE = -10;
-    public static final float VIEWPORT_HEIGHT = 10;
+    public static final float VIEWPORT_HEIGHT = 12;
     public static final float VIEWPORT_WIDTH = 12f / 9f * VIEWPORT_HEIGHT;
 }
