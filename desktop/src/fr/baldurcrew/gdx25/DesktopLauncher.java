422eed235467cd07c3db2bfd40a0a848f6e7efb3
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-06-23 10:05:56 +0200

    Set desktop app to 1600x900

--- desktop/src/fr/baldurcrew/gdx25/DesktopLauncher.java
+++ desktop/src/fr/baldurcrew/gdx25/DesktopLauncher.java
@@ -5,10 +5,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("gdx-jam-25");
-		new Lwjgl3Application(new CoreGame(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("gdx-jam-25");
+        config.setWindowedMode(1600, 900);
+        new Lwjgl3Application(new CoreGame(), config);
+    }
 }
