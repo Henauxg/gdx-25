919a7d3b2e18cb0bd736adbbf4fbf28fed1cc426
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-06-25 15:31:04 +0200

    Customize the libGDX loading screen for the html target

--- html/src/fr/baldurcrew/gdx25/client/HtmlLauncher.java
+++ html/src/fr/baldurcrew/gdx25/client/HtmlLauncher.java
@@ -3,20 +3,39 @@ package fr.baldurcrew.gdx25.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
 import fr.baldurcrew.gdx25.CoreGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+        return new GwtApplicationConfiguration(true);
+        // Fixed size application:
+        //return new GwtApplicationConfiguration(480, 320);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new CoreGame();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new CoreGame();
+    }
+
+    @Override
+    public Preloader.PreloaderCallback getPreloaderCallback() {
+        return createPreloaderPanel(GWT.getHostPageBaseURL() + "dftk_thumbnail_small.jpg");
+    }
+
+    @Override
+    protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+        meterPanel.setStyleName("gdx-meter");
+        meterPanel.addStyleName("nostripes");
+        meterStyle.setProperty("backgroundColor", "##1f1f1f");
+        meterStyle.setProperty("backgroundImage", "none");
+        meterStyle.setProperty("backgroundImage", "none");
+    }
+
 }
