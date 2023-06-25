6cd26c9b8ad2dc6c5cf4b019e914bc25dd5d3389
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-06-25 23:24:05 +0200

    Allow for a greater X distance before terminating the player

--- core/src/fr/baldurcrew/gdx25/character/Character.java
+++ core/src/fr/baldurcrew/gdx25/character/Character.java
@@ -300,7 +300,7 @@ public class Character implements Disposable, ContactHandler { // TODO Remove Ac
             isAlive = false;
             game.aiCharacterDied(this);
         }
-        if (isAlive && !aiControlled && Math.abs(getX() - Constants.VIEWPORT_WIDTH / 2f) >= Boat.BOAT_WIDTH) {
+        if (isAlive && !aiControlled && Math.abs(getX() - Constants.VIEWPORT_WIDTH / 2f) >= 1.5f * Boat.BOAT_WIDTH) {
             isAlive = false;
             game.playerDied();
         }
